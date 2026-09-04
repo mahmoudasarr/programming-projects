88c8762b86e46e7cbdda0fd3feae5f0f50badc14
Mahmoud Asar <prowork.reconcile111@simplelogin.com>
2026-09-04 16:02:14 +0300

    add feutuers

--- python/flaskapp/app.py
+++ python/flaskapp/app.py
@@ -3,15 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 lessons = [{
     "title" : "Request",
-    "Course" : "Python",
-    "Author" : "Omar"
-}]
+    "course" : "Python",
+    "author" : "Omar"
+},
+{
+    "title" : "Request-HTML Library Course",
+    "course" : "Python",
+    "author" : "Ahmed"
+},
+{
+    "title" : "Detetime Module",
+    "course" : "Python",
+    "author" : "Mohamed"
+},
+]
 
 @app.route("/")
 @app.route("/home")
 
 def home():
-    return render_template("home.html")
+    return render_template("home.html", lessons=lessons)
 
 @app.route("/about")
 def about():
